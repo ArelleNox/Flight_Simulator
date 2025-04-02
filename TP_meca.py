657871313a74ddd79426a0e084c061b999025174
Arellenox <jgrumiaux@gmail.com>
2025-04-02 09:54:03 +0200

    step 1

--- TP_meca.py
+++ TP_meca.py
@@ -1,2 +1,29 @@
 import math as mt
 import matplotlib.pyplot as plt
+
+###FONCTION
+def matrice_translation(vecteur_translation):
+    if len (vecteur_translation) != 3:
+        raise ValueError("Le vecteur de translation doit contenire 3 element (a, b, c).")
+    
+    a, b, c= vecteur_translation
+    
+    matriceT = [
+        [1, 0, 0, a],
+        [0, 1, 0, b],
+        [0, 0, 1, c],
+        [0, 0, 0, 1]
+    ]
+
+    for row in matriceT:
+        print(row)
+    
+    return matriceT
+
+
+    # print("n\--- Matrice de translation---")
+    # print("T=", matriceT)
+    # return matriceT
+
+
+###INTERFACE
