a099a207cdf3469a246c94f517fc9f42065f5eb4
Arellenox <jgrumiaux@gmail.com>
2025-04-07 09:47:09 +0200

    rota combiné

--- TP_meca.py
+++ TP_meca.py
@@ -2,6 +2,7 @@ import math as mt
 import matplotlib.pyplot as plt
 
 ###FONCTION
+#etape 1
 def matrice_translation(vecteur_translation):
     if len (vecteur_translation) != 3:
         raise ValueError("Le vecteur de translation doit contenire 3 element (a, b, c).")
@@ -20,6 +21,7 @@ def matrice_translation(vecteur_translation):
 
     return matriceT
 
+#etape 2 & 4
 def matrice_rotation(vecteur_rotation):
     if len (vecteur_rotation) != 3:
         raise ValueError("Le vecteur de rotation doit contenire 3 element (x, y, z).")
@@ -47,19 +49,13 @@ def matrice_rotation(vecteur_rotation):
             [0, 0, 0, 1]
         ]
 
-    # print("n\--- Matrice de rotation---")
-    # for row in matriceRX and matriceRY and matriceRZ:
-    #     print(row)
-    
-    # return matriceRX, matriceRY, matriceRZ
+    rotation_combinée = produit_matrice(matriceRZ, matriceRY)
+    rotation_combinée2 = produit_matrice(rotation_combinée, matriceRX)
+
+    return rotation_combinée2
 
 
-    print("--- Matrice de rotation---")
-    print("Matrice de Rx=", matriceRX)
-    print("Matrice de Ry=", matriceRY)
-    print("Matrice de Rz=", matriceRZ)
-
-
+#etape 3
 def produit_matrice(A, B):
     if len(A[0]) != len(B):
         raise ValueError("Le nombre de colonne de A doit être égale au nombre de ligne de B.")
@@ -71,7 +67,6 @@ def produit_matrice(A, B):
             for k in range(len(B)):
                 C[i][j] += A[i][k] * B[k][j]
 
-    print("\n--- Produit de matrices ---")
     print("A x B = ", C)
     
     return C
@@ -84,9 +79,11 @@ T = [3,4,2]
 matrice_translation(T)
 print(" ")
 
+print("Matrice de rotation")
 R = ([mt.pi, mt.pi/2, mt.pi/4])
 matrice_rotation(R)
 
+print("Produit de matrice")
 A = [
 [1, 2, 0, 3],
 [0, 0, 0, 2],
